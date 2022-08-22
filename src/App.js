ac97f4c2159b7234b592177b410c8ba3a3b54e2a
Rancune <3519851+Rancune@users.noreply.github.com>
2022-08-22 11:54:31 +0200

    scss styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/styles/App.scss";
+import logo from "./assets/images/cookchef-d.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <ul>
+        <button>Whislist</button>
+        <button>Connexion</button>
+      </ul>
+    </header>
   );
 }
 
