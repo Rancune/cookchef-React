9d0574e83e03e6efb559e270aa279ecf846cd88f
Rancune <3519851+Rancune@users.noreply.github.com>
2022-09-05 10:47:05 +0200

    Authentification  setup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./assets/styles/index.scss";
-import App from "./App";
-import { ApiContext } from "./context/ApiContext";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/index.scss';
+import App from './App';
+import { ApiContext } from './context/ApiContext';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-if (module.hot) {
-  module.hot.accept();
-}
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ApiContext.Provider value="http://localhost:3333/api/recipes">
-        <App />
-      </ApiContext.Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<ApiContext.Provider value="http://localhost:3333/api/recipes">
+				<App />
+			</ApiContext.Provider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
